ea7208710b2ab616170a5a256c17a5560290a6a9
Bruno Cataldo <bruno.cataldo@outlook.com>
2018-07-27 01:24:58 -0300

    Aplicação novo layout e inclusão da biblioteca angular.material no lugar do bootstrap

--- src/app/shared/component.shared.ts
+++ src/app/shared/component.shared.ts
@@ -1,6 +1,9 @@
 import { MainReportComponent } from '../reports/main/main-report.component';
+import { SidebarComponent } from './sidebar-component/sidebar.component';
 
 export const COMPONENTS = [
+    SidebarComponent,
+    
     MainReportComponent
 ];
 
