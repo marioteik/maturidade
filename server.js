ce59e9e89edd835cce4d5eb4c3ca0a38f9a53116
marioteik <mario.alberto.santos@avanade.com>
2018-07-31 18:42:50 -0300

    correção do nome da pasta gerada pela build de prod

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/maturidade4-dx'));
+app.use(express.static(__dirname + '/dist/Maturidade4DX'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/maturidade4-dx/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Maturidade4DX/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
